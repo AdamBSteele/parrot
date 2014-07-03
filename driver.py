7293e531ed64b928248e95eb0ab9fa452fba0123
adambsteele <as09am@my.fsu.edu>
2014-07-03 19:50:49 -0400

    Creating classes

--- driver.py
+++ driver.py
@@ -1,22 +1,27 @@
-import os
-PARROTPATH = os.getenv('PARROTPATH')
-os.chdir(PARROTPATH)
+from parrotSuite import *
 import echo
 import squawk
 import metrics
-from parrotSuite import *
-
-SETTINGS = getSettings(PARROTPATH + 'config.txt')
 
 def main():
-	for bot in [x for x in os.listdir(PARROTPATH) if os.path.isdir(x) and 'git' not in x]:
-		userSettings = getUserSettings(PARROTPATH + bot + '/parrot.cfg')
-		getTimeline(userSettings)
-		echo.run(userSettings)
-		squawk.run(userSettings)
-		metrics.run(userSettings)
+    if SETTINGS.get('DEBUG') == "TRUE":
+        for x in SETTINGS.keys():
+            print (x + ": " + SETTINGS[x])
 
+    for bot in botList():
+        fullPath = PARROTPATH + bot + '/'
+        user = User(fullPath)
+        if SETTINGS.get('DEBUG') == "TRUE":
+            print user
+        # user = User(userSettings, bot)
+        # getTimeline(userSettings)
+        # echo.run(userSettings)
+        # squawk.run(userSettings)
+        # metrics.run(userSettings)
+
+def botList():
+    return [x for x in os.listdir(PARROTPATH) if os.path.isdir(x) and 'git' not in x]
 
 if __name__ == '__main__':
-	print "START"
-	main()
+    print "START"
+    main()
--- parrotSuite.py
+++ parrotSuite.py
@@ -1,27 +1,67 @@
-class Timeline:
-	def __init__(t):
-		self.t = t
-		self.statuses = []
-		self.lastStatus = None
+import os
+import Twitter
+
+# Set path
+PARROTPATH = os.getenv('PARROTPATH')
+os.chdir(PARROTPATH)
+
+# Grab settings from path
+SETTINGS = {}
+for line in open(PARROTPATH + 'config.txt'):
+    if '//' not in line[0:1]:
+        x,y = line.split('=')
+        SETTINGS[x.strip()] = y.strip()
+
+class User:
+    def __init__(self, path):
+        self.path = path
+
+        settings = {}
+        for line in open(self.path + 'parrot.cfg'):
+            x,y = line.split('=')
+            settings[x.strip()] = y.strip()
+
+            self.settings =  settings
+        self.initializeTwitter()
+
+    def initializeTwitter(self):
+        OTOKEN = SETTINGS['OTOKEN']
+        OSECRET = SETTINGS['OSECRET']
+        CONSKEY = self.settings['CONSKEY']
+        CONSSECRET = self.settings['CONSSECRET']
+        # Initialize twitter
+        try:
+            t = Twitter(
+                auth=OAuth(OTOKEN, OSECRET, CONSKEY, CONSSECRET)
+            )
+        except Exception as e:
+            print "Twitter failed"
+            print str(e)
+            exit
+        print "TWITTER SUCCESS"
+
+    def __repr__(self):
+        res = "PATH: " + self.path
+
+        res += "\nSettings: "
+        for x in self.settings.keys():
+            res += '\n  ' + x + ": " + self.settings[x]
+
+        return res
+
+
+
+
+def getTimeline(settings):
+    pass
 
 
 class Status:
-	def __init__(status):
-		self.text = status.get('text')
+    def __init__(status):
+        self.text = status.get('text')
 
-def getSettings(filename):
-	settings = {}
-	for line in open(filename):
-		x,y = line.split('=')
-		settings[x.strip()] = y.strip()
-	return settings
-
-def getUserSettings(filename):
-	settings = {}
-	for line in open(filename):
-		x,y = line.split('=')
-		settings[x.strip()] = y.strip()
-	return settings
-
-def getTimeline(userSettings):
-	pass 	
+class Timeline:
+    def __init__(t):
+        self.t = t
+        self.statuses = []
+        self.lastStatus = None
